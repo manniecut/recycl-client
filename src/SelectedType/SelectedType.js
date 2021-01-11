c243dc69037f30afa915ef795435d9d3035ebe7f
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2021-01-11 03:43:31 -0500

    style SelectedType

--- src/SelectedType/SelectedType.js
+++ src/SelectedType/SelectedType.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PopUpScheduler from '../PopUp/PopUpScheduler';
-import './SelectedType.css'
+import './SelectedType.css';
 
 // this component is displays the details for recycling a specfic type of item as well as the option to schedule a pickup
 
@@ -23,13 +23,12 @@ class SelectedType extends Component {
         return (
             <div className='selectedtype__screen'>
                 <h3>{this.state.selectedType}</h3>
-                
-                <div className='selectedtype__locations'>
-                    <p>Places near you that might take {this.state.selectedType}:</p>
-                    <iframe title='recycle-centers' src={`https://www.google.com/maps/embed/v1/place?key=${this.state.mapsKey}&q=${query}`}></iframe>
+                <div className={`selected_${this.state.selectedType}`}>
+                    <div className='selectedtype__locations'>
+                        <p>Places near you that might take <span className='colortext'>{this.state.selectedType}</span>:</p>
+                        <iframe title='recycle-centers' src={`https://www.google.com/maps/embed/v1/place?key=${this.state.mapsKey}&q=${query}`}></iframe>
+                    </div>
                 </div>
-                <h2>- or -</h2>
-                
                 <button onClick={e => this.togglePopup()}>Schedule a Pickup</button>
                 {this.state.showPopUp ?
                     <PopUpScheduler
